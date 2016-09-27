4d6c94d52d3d7ff83f197df095f3b8f5b4bb0bc6
adi2342 <aditya14all@gmail.com>
2016-09-27 11:05:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var counter = 0;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
