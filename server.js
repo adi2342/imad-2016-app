001e08bafef229c493ea7f043ca99b1868b5d3d7
adi2342 <aditya14all@gmail.com>
2016-11-08 16:00:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var counter = 0;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+// When user requests for form to fill details about help he can offer, this file is fetched
+app.get('/list-help', function(req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'listhelp.html'));
+})
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
