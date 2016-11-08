284bdad2e5eca578a61da0d305cf04397bd94c3e
adi2342 <aditya14all@gmail.com>
2016-11-08 17:31:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById("button");
 
 element.onclick = function(){
-  location.href = 'http://adi2342.imad.hasura-app.io/listhelp'
+  location.href = 'http://adi2342.imad.hasura-app.io/list-help'
 }
 
 console.log('Finished')
