501a5b588c716f3eb9db086af07fa36b9883c3fc
adi2342 <aditya14all@gmail.com>
2016-11-09 13:01:24 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,9 +1,9 @@
 var element = document.getElementById('submit-button');
 
 element.onclick = function() {
- var email = document.getElementById('email');
- var password = document.getElementById('password');
- var address = document.getElementById('address');
- console.log(email.value);
+ var email = document.getElementById('email').value;
+ var password = document.getElementById('password').value;
+ var address = document.getElementById('address').value;
+ console.log(email);
 }
 
