cfc458755d719ab0cd6fe98992553b28a888035b
adi2342 <aditya14all@gmail.com>
2016-11-10 11:41:40 +0530

    Create main3.js

---
+++ main3.js
@@ -0,0 +1,25 @@
+var element = document.getElementById('submit-button');
+
+element.onclick = function() {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readystate === XMLHttpRequest.DONE) {
+            if(request.status === 200)
+            console.log('Posted successfully');
+            else(request.status === 500)
+            console.log('Something went wrong with the server');
+        }
+    }
+
+// Making a request
+ var details = document.getElementById('details').value;
+ var phone = document.getElementById('phone').value;
+ var address = document.getElementById('address').value;
+ var name = document.getElementById('name').value;
+ console.log(name);
+ request.open('POST','http://adi2342.imad.hasura-app.io/create-help',true);
+ request.setRequestHeader('Content-type', 'Application/JSON');
+ request.send(JSON.stringify({"details" : "details", "phone" : "phone", "name" : "name", "address" : "address"}));
+
+}
