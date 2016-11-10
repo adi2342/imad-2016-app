9098e5186cf99ae77e7f64084dbba78bab4ee035
adi2342 <aditya14all@gmail.com>
2016-11-10 18:55:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.get('/create-help', function(req,res){
   var phone = req.query.phone;
   var address = req.query.address;
   var details = req.query.details;
+  res.send(address);
     pool.query('INSERT INTO supply (name) VALUES ($1)', [name], function(err){
     if(err) return onError(err);
     else {
